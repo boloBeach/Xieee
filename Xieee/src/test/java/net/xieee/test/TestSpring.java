32b564d6ddabe692f0fe9876659f8bb23b3bd631
bolobeach <bolobeach@gmail.com>
2015-01-11 22:11:48 +0800

    commit test Spring

--- Xieee/src/test/java/net/xieee/test/TestSpring.java
+++ Xieee/src/test/java/net/xieee/test/TestSpring.java
@@ -2,11 +2,14 @@ package net.xieee.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class TestSpring {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
-				"applicationContext.xml");
+		ApplicationContext context = new FileSystemXmlApplicationContext(
+				"C:\\Users\\zhangbing\\git\\Xieee\\Xieee\\src\\main\\webapp\\WEB-INF\\applicationContext.xml");
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) context
+				.getBean("jdbcTemplate");
+		System.out.println(jdbcTemplate);
 	}
 }
