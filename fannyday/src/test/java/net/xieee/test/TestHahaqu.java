1561b0265c2a08b68e397c817b26dc9eb2422b35
bolobeach <bolobeach@gmail.com>
2015-03-17 14:46:29 +0800

    修改testHahaqu

--- fannyday/src/test/java/net/xieee/test/TestHahaqu.java
+++ fannyday/src/test/java/net/xieee/test/TestHahaqu.java
@@ -2,12 +2,26 @@ package net.xieee.test;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import net.xieee.spider.util.Constants;
 import net.xieee.spider.util.DownloadImage;
 import net.xieee.spider.util.HahaquGodReply;
+import net.xieee.util.StringUtil;
 import net.xieee.web.bean.Picture;
+import net.xieee.web.service.impl.PictureServiceImpl;
 
 public class TestHahaqu {
+	
+	public static PictureServiceImpl pictureServiceImpl;
+	public TestHahaqu(){
+		// 首先判断是否已经被下载了，如果被下载了，那么直接就返回。通过获取xml文件才做。
+		String xmlPath = DownloadImage.class.getClassLoader().getResource(Constants.springXMLName).getPath();
+		ApplicationContext context = new FileSystemXmlApplicationContext("/"+xmlPath);
+		pictureServiceImpl = (PictureServiceImpl) context.getBean("pictureServiceImpl");
+	}
+	
 	public static void main(String[] args) throws Exception{
 		HahaquGodReply hahaquGodReply = new HahaquGodReply();
 		DownloadImage downloadImage = new DownloadImage();
@@ -18,7 +32,11 @@ public class TestHahaqu {
 			List<Picture> pictures = hahaquGodReply.getPicturesByHahaqu(url, host, host,"http://www.hahaqu.com/");
 			for (Picture picture : pictures) {
 				System.out.println(picture.toString());
-				downloadImage.Download(picture, host,Constants.god_reply_img_save_path,Constants.god_img_http_path);
+				if(StringUtil.isNull(picture.getPicture_name())){
+					pictureServiceImpl.savePicture(picture);
+				}else{
+					downloadImage.Download(picture, host,Constants.god_reply_img_save_path,Constants.god_img_http_path);
+				}
 			}
 		}
 	}
