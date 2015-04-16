0464dc462394ee3181cd570cd62931c9d3c126b0
bolobeach <bolobeach@gmail.com>
2015-04-16 21:26:02 +0800

    修改内涵吧数据

--- Spider/src/net/xieee/test/TestNeiHanBaJokePicture.java
+++ Spider/src/net/xieee/test/TestNeiHanBaJokePicture.java
@@ -18,12 +18,13 @@ public class TestNeiHanBaJokePicture {
 		String referer = "http://www.neihan8.com/s/";
 		String httpHost = "http://www.neihan8.com";
 		for (int i = 0; i < 2; i++) {
-			url = "http://www.neihan8.com/s/data.php?start="+i+"&num=1&type=3&markid=undefined&typeid=63449&hotdate=14&tagtid=2";
+			url = "http://www.neihan8.com/s/data.php?start="+i+"&num=1&type=0&markid=undefined&typeid=0&hotdate=14&tagtid=2";
 			try {
 				Picture picture = neiHanJokePicture.getPicturesByNeiHan(url, referer, host, httpHost);
 				logger.warn(picture.toString());
 				downloadImage.Download(picture, host,Constants.joke_picture_save_path,Constants.imgHTTPPath);
 			} catch (Exception e) {
+				e.printStackTrace();
 				logger.error(e.toString());
 			}
 		}
